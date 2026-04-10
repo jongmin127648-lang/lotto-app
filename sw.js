19e2c043cc9a5659d8061055a961347794040dba
jongmin127648-lang <jongmin127648@gmail.com>
2026-04-10 13:25:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,23 +2,29 @@ const CACHE_NAME = "lotto-v2";
 
 const FILES_TO_CACHE = [
   "./",
-  "./index.html"
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
+/* ✅ 설치 */
+self.addEventListener("install", (e) => {
   self.skipWaiting();
+
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
-self.addEventListener("activate", e => {
+/* ✅ 활성화 (이전 캐시 삭제) */
+self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys => {
+    caches.keys().then((keys) => {
       return Promise.all(
-        keys.map(key => {
+        keys.map((key) => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
@@ -28,8 +34,21 @@ self.addEventListener("activate", e => {
   );
 });
 
-self.addEventListener("fetch", e => {
+/* ✅ 요청 처리 (오프라인 지원 강화) */
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .then((res) => {
+        // 정상 응답이면 캐시에 저장
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, clone);
+        });
+        return res;
+      })
+      .catch(() => {
+        // 오프라인이면 캐시에서 반환
+        return caches.match(e.request);
+      })
   );
 });
