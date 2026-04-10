d4d4291c6399b13a4310a47a2e4a5d749741cd20
jongmin127648-lang <jongmin127648@gmail.com>
2026-04-10 12:29:43 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+// sw.js
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("lotto").then(cache => {
+      return cache.addAll(["./", "./index.html"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
